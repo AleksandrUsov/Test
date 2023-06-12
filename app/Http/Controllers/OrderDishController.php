1a70b232d485ed04a6fb94cb0f7e4a2f9c69bdd0
Aleksandr Usov <usov.als579@gmail.com>
2023-06-13 00:40:10 +0700

    Создание миграций

---
+++ app/Http/Controllers/OrderDishController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order_dish;
+use Illuminate\Http\Request;
+
+class OrderDishController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order_dish $order_dish)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Order_dish $order_dish)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order_dish $order_dish)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order_dish $order_dish)
+    {
+        //
+    }
+}
